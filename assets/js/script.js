2c1b8a5ad30beb933a0d8c8a47a884a68521ce47
dardinierjames <dardinier.james@orange.fr>
2020-11-06 15:21:37 +0100

    emptyRemove

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('#firstText').empty();
+// la fonction empty va vider l'élément de son contenu
+    $('#secondText').remove();
+// et la fonction remove va supprimer l'élément et son contenu
+});
